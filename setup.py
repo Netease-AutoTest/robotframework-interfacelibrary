7d755a1b15dd8cb1dc5c38ca44f799e06648049f
seven57 <seven_five57@sina.com>
2016-07-29 14:31:45 +0800

    InterfaceLibrary -> src/InterfaceLibrary

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@
 import setuptools
 import os
 print os.popen("pip install http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-2.0.4.zip#md5=3df394d89300db95163f17c843ef49df").read()
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+setuptools.setup(setup_requires=['pbr'], pbr=True,package_dir = {'': 'src'},packages=["InterfaceLibrary"])
