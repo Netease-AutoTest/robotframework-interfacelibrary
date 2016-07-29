398a29d009a96ca66e8b9fe83f03eaedef6a9a41
seven57 <seven_five57@sina.com>
2016-07-29 11:07:09 +0800

    os.popen("pip install mysql-connector-python") manually in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 import setuptools
+import os
+print os.popen("pip install http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-2.0.4.zip#md5=3df394d89300db95163f17c843ef49df").read()
 setuptools.setup(setup_requires=['pbr'], pbr=True)
